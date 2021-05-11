9a90087d39e0114c412504ce99636a9bfb92ca14
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-05-11 11:46:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,7 @@
-
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+a=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
+a
+st.sidebar.title("visualization selector")
